0261a9e3ceba1e6d1fec9edc7346fdeed634cb38
Ivan Kirillov <van-kirillov@yandex.ru>
2021-11-14 00:18:56 +0300

    Добавить реалиизация репозитория книг в памяти

---
+++ infrastructure/bookShop.Memory/BookRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Store;
+using System.Linq;
+
+namespace bookShop.Memory
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly Book[] books = new[]
+        {
+            new Book(1, "Война и мир"),
+            new Book(2, "Пушкин"),
+            new Book(3, "Программирование "),
+            new Book(4, "Горький"),
+        };
+        public Book[] GetAllByTitle(string titlePart)
+        {
+            return books.Where(book => book.Title.Contains(titlePart)).ToArray();
+        }
+    }
+}
